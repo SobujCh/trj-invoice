79362b70bc4d3545f7281d7b2bc3183f563a17c0
Sobuj Chowdhury <sobujos@gmail.com>
2025-07-31 04:16:55 +0600

    feat: add puppeteer-real-browser dependency and initialize connection in process2.js

---
+++ process2.js
@@ -0,0 +1,12 @@
+const { connect } = require("puppeteer-real-browser");
+const start = async () => {
+  const { page, browser } = await connect();
+    await page.goto("https://payment.ivacbd.com/");
+    //here is the page title
+    const title = await page.title();
+    console.log("Page title:", title);
+    //close the browser
+    await browser.close();
+};
+
+start();
