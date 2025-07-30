eb76d41cbcf0eeda1224e5cbdc2c74ebbb34bb26
Sobuj Chowdhury <sobujos@gmail.com>
2025-07-31 04:40:53 +0600

    refactor: improve logging during Puppeteer connection setup in process2.js

--- process2.js
+++ process2.js
@@ -1,10 +1,11 @@
 const { connect } = require("puppeteer-real-browser");
 const start = async () => {
-    const { page, browser } = await connect({
-        headless: true
-    });
+    console.log("Starting Puppeteer Real Browser...");
+    const { page, browser } = await connect();
+    console.log("Connected to browser.");
     await page.goto("https://payment.ivacbd.com/");
     //here is the page title
+    console.log("Navigated to page.");
     const title = await page.title();
     console.log("Page title:", title);
     //close the browser
